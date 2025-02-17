42dad7558e23f7f607f1a4456e4909efd0221946
riiz0 <shawnrizo@hotmail.com>
2025-02-17 21:42:27 +0000

    Feat: Update Dependencies and Configs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const privateKey = process.env.PRIVATE_KEY || "";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://sonic-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+        blockNumber: 223528000,
+      },
+    },
+  },
 };
 
 export default config;
